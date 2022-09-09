f6e566f14efa20ae1b9aa666326dde89a1f01616
Nwakasiikenna <nwakasiikenna2000@gmail.com>
2022-09-09 16:46:41 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - is Printing
-*
-* Return: Always 0 (Very Successful)
-*/
+ * main - is Printing
+ *
+ * Return: Always 0 (Very Successful)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual
+	puts("\"Programming is like building a multilingual
 puzzle");
-return (0);
+	return (0);
 }
