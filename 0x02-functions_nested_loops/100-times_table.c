cd7550d184ca8ef805a84b78a1e7bc0c86f245ff
Nwakasiikenna <nwakasiikenna2000@gmail.com>
2022-09-14 01:37:24 -0700

    commit 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,18 +1,70 @@
 #include "main.h"
 
 /**
- * main - Will check the code written.
- *
- * Return: Always 0.
+ * number - func that print number
+ * @i: number to print
+ * Return: void
  */
-int main(void)
+
+void number(int i)
 {
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
+	if (i / 100 != 0)
+	{
+		_putchar(i / 100 + '0');
+		_putchar(((i % 100) / 10) + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		if (i / 10 == 0)
+		{
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(i / 10 + '0');
+		}
+	}
+	_putchar(i % 10 + '0');
+}
+
+/**
+ * print_times_table - a function that print the n times table, starting with 0
+ *@num: the parameter of the func
+ * Return: void
+ */
+
+void print_times_table(int num)
+{
+	int n;
+	int t;
+	int m;
+
+	if (num < 0 || num > 15)
+	{
+		return;
+	}
+	if (num == 0)
+	{
+		_putchar(48);
+		_putchar('\n');
+		return;
+	}
+	for (n = 0; n <= num; n++)
+	{
+		_putchar(48);
+		_putchar(',');
+		_putchar(' ');
+		for (t = 1; t <= num; t++)
+		{
+			m = n * t;
+			number(m);
+			if (t != num)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
